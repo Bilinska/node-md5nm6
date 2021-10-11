f7a4f899d843f9d8eebe5d924899fba3f2b3a535
Bilinska <bilinska.iryna.02@gmail.com>
2021-10-11 13:28:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-md5nm6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-md5nm6)
---
+++ index.js
@@ -0,0 +1,17 @@
+function splitInteger(num, parts) {
+  const result = [];
+  const divisor = Math.floor(num / parts);
+  const rest = num % parts;
+
+  for (let i = 0; i < parts - rest; i++) {
+    result.push(divisor);
+  }
+
+  for (let i = 0; i < rest; i++) {
+    result.push(divisor + 1);
+  }
+
+  return result;
+}
+console.log(splitInteger(100, 25));
+module.exports = splitInteger;
